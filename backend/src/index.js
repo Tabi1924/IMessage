86bbba35dbdd21bcbd20cd5ac065b982b7078bb4
Tabish1924 <tabishkittur@gmail.com>
2026-08-24 12:52:36 +0530

    Add middleware for CORS and Clerk, and implement health check endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,22 @@
 import express from "express";
 import "dotenv/config";
+import { clerkMiddleware } from '@clerk/express';
+import cors from "cors";
+import User from "./models/User.js";
+import { connectDB } from "./lib/db.js";
 
 
 const app = express();
 const PORT = process.env.PORT;
+const FRONTED_URL=process.env.FRONTED_URL;
+app.use(express.json())
+app.use(cors({origin:FRONTED_URL, credentials:true}));
+app.use(clerkMiddleware())
+app.get("/health", (req, res) => {
+    res.status(200).json({ok: true})
+});
 
-app.listen(PORT, () => console.log("Server rat  30000", PORT));
+app.listen(PORT, () => {
+    connectDB();
+     console.log("Server rat  30000", PORT)
+    });
